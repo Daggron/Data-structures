522b03b78ee508343635fea87463593c27f8e361
Daggron <returnofking04@gmail.com>
2019-09-25 00:14:16 +0530

    Updated now string is returning instead of printing

--- strings/integer-to-roman.cpp
+++ strings/integer-to-roman.cpp
@@ -3,23 +3,25 @@ using namespace std;
 // #include<stdlib.h>
 // #include<string.h>
 
-void conversion(int num){
+string conversion(int num){
 
   int number[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
   string roman[] = {"I" ,"IV","V" , "IX", "X" , "XL" , "L" ,"XC" , "C" , "CD", "D" , "CM" ,"M"};
 
   int i =12;
+  string res;
 
   while(num>0){
     int div = num / number[i];
     num = num % number[i];
 
     while(div--){
-      cout<<roman[i];
+      res+=roman[i];
     }
     i--;
   }
 
+  return res;
 }
 
 
@@ -27,5 +29,6 @@ int main(){
     int number;
     printf("Enter The number\n");
     scanf("%d",&number);
-    conversion(number);
+    string a = conversion(number);
+    cout<<a;
 }
